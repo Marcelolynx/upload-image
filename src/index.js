953b1e41d3edead7bbb4e2a267501fa0aafdd7a7
Marcelo Oliveira <marcelolynx@gmail.com>
2019-11-18 09:55:57 -0400

    backend do sistema de upload de imagem finalizado

---
+++ src/index.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const mongoose = require('mongoose');
+const app = express();
+
+
+/*
+Database Setup
+*/
+mongoose.connect (
+    process.env.MONGO_URL,
+    {
+        useNewUrlParser: true
+
+    }
+);
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(
+        "/files",
+        express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+    );
+
+app.use(require('./routes'));
+
+app.listen(3000);
